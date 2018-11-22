d3632f74cf71ce75efe14e627bc6c9d7e760f362
Admin <toanoc434@gmail.com>
2018-11-22 10:19:41 +0700

    update 22/11

---
+++ app/src/main/java/com/fpoly/dell/project1/ui/KinhNghiemActivity.java
@@ -0,0 +1,31 @@
+package com.fpoly.dell.project1.ui;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+
+import com.fpoly.dell.project1.R;
+
+public class KinhNghiemActivity extends AppCompatActivity {
+        private WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kinh_nghiem);
+
+        setTitle("Kinh Nghiệm");
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        webView= findViewById(R.id.wb_kinhkngiem);
+        String url = "http://nguoichannuoi.vn/";
+
+        webView.getSettings().setLoadsImagesAutomatically(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        webView.loadUrl(url);
+    }
+}
---
+++ app/src/main/java/com/fpoly/dell/project1/ui/ManHinhChaoActivity.java
@@ -0,0 +1,29 @@
+package com.fpoly.dell.project1.ui;
+
+import android.content.Intent;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.fpoly.dell.project1.R;
+
+public class ManHinhChaoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manhinhchao);
+        CountDownTimer countDownTimer = new CountDownTimer(2000,2000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                finish();
+                startActivity(new Intent(getApplicationContext(),HomeActivity.class));
+            }
+        }.start();
+    }
+}
