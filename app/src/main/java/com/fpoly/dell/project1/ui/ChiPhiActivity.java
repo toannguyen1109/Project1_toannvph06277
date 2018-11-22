fbd51ecc90ebd147155a54841be3ab6c52333eb6
Admin <toanoc434@gmail.com>
2018-11-22 10:23:24 +0700

    update

---
+++ app/src/main/java/com/fpoly/dell/project1/ui/ChiPhiActivity.java
@@ -0,0 +1,15 @@
+package com.fpoly.dell.project1.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.fpoly.dell.project1.R;
+
+public class ChiPhiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chi_phi);
+    }
+}
